c175f77b60c9626896e7e0395f35e56d4c42ef06
rokuosan <85651386+rokuosan@users.noreply.github.com>
2021-12-04 18:49:01 +0900

    variable.mdを作成

---
+++ src/assets/program/variable/wallet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    // 変数名walletで変数を宣言する
+    float wallet;
+
+    // 変数walletに値を入れる。
+    wallet = 1234.5;
+
+    // 変数walletを表示する
+    printf("財布の中身: %f\n", wallet);
+
+    return 0;
+}
